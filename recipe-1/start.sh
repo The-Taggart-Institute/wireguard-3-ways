cccf7f6c921827818c35d103b53ef93d88aa2793
Taggart <mtaggart@taggart-tech.com>
2025-08-05 21:07:26 -0700

    Build images and setup WG on launch

--- recipe-1/start.sh
+++ recipe-1/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Build images
+podman compose build wg-home
+podman compose build lighthouse
+
 # Launch the containers
 podman compose up -d
 
+# Get configs initialized
+for i in $(podman container ls | grep "recipe-1" | cut -d " " -f 1); do
+  podman container exec $i /peer/setup.sh
+done
 
 # Initialize Zellij
 zellij -n zellij_recipe-1.kdl -s recipe-1
