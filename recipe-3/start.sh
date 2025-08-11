0b86a1dd637084524409b516b51f8e707f67e7f1
Taggart <mtaggart@taggart-tech.com>
2025-08-11 15:46:59 -0700

    Create Headscale cert, set up routes

--- recipe-3/start.sh
+++ recipe-3/start.sh
@@ -2,6 +2,13 @@
 
 HEADSCALE_IP=10.1.99.11
 
+cd headscale
+openssl req -x509 -newkey rsa:4096 -keyout config/headscale.key -out config/headscale.crt -days 365 -nodes \
+  -subj "/CN=headscale" \
+  -addext "subjectAltName=DNS:r3_headscale,IP:$HEADSCALE_IP"
+cp config/headscale.crt ../client/
+cd ..
+
 # Build images
 podman compose build home-router
 podman compose build headscale
@@ -19,8 +26,11 @@ roaming_key=$(podman container exec recipe-3_headscale headscale preauthkeys cre
 
 # Login Tailscale
 echo "[+] Standing up Tailscale on clients"
-podman container exec recipe-3_ts-client_home tailscale up --login-server http://$HEADSCALE_IP:8080 --auth-key $home_key --advertise-routes=192.168.99.0/24
-podman container exec recipe-3_ts-client_roaming tailscale up --login-server http://$HEADSCALE_IP:8080 --auth-key $roaming_key
+podman container exec recipe-3_ts-client_home tailscale up --login-server https://$HEADSCALE_IP --auth-key $home_key --advertise-routes=192.168.99.0/24
+# Approve routes
+podman container exec recipe-3_headscale headscale nodes approve-routes -i 1 -r 192.168.99.0/24
+podman container exec recipe-3_ts-client_roaming tailscale up --login-server https://$HEADSCALE_IP --auth-key $roaming_key --accept-routes
+
 
 # Set up tailscaled on our tailscale clients
 # for i in $(podman container ls | grep ts-client | cut -d " " -f 1); do
