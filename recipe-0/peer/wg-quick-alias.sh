e7bbda0bd583a35869853b62899af5800b817439
Taggart <mtaggart@taggart-tech.com>
2025-08-12 23:36:57 -0700

    Rebuild: Tue Aug 12 11:36:57 PM PDT 2025

--- recipe-0/peer/wg-quick-alias.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-alias wg-quick="bash wg-quick"
--- recipe-0/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Bring down the containers
-podman compose down -t 0
-
-# Kill the Zellij Session
-zellij delete-session --force recipe-0
